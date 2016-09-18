d58867aa3ff65c38906b400c405681e4b5edff44
DeveloperTommy <its.tommy.nguyen@gmail.com>
2016-09-18 12:16:51 -0400

    Implemented test for median

--- test/BasicStatsTest.java
+++ test/BasicStatsTest.java
@@ -17,11 +17,31 @@ public class BasicStatsTest {
 
     @Test
     public void testMedian() {
+      //Median should be 8.0 since size is even
+      /* double[] numbers = {1, 4, 7, 9, 11, 21}; */
+      double[] numbers = {9, 11, 1, 4, 7, 21};
 
+      double median = BasicStats.median(numbers);
+      assertEquals(8.0, median, EPS);
+
+      //Median should be 7 since size is odd
+      double[] numbers2 = {9, 1, 4, 7, 21};
+      median = BasicStats.median(numbers2);
+      assertEquals(7, median, EPS);
+
+      //Median should be 3 since size is 1
+      double[] numbers3 = {3};
+      median = BasicStats.median(numbers3);
+      assertEquals(3, median, EPS);
+
+      //Median should be 0 since size is 0
+      double[] numbers4 = {};
+      median = BasicStats.median(numbers4);
+      assertEquals(0, median, EPS);
     }
 
     @Test
     public void testMode() {
-      
+
     }
 }

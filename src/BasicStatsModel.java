79c45fdc9efe96ffeb1ee67893d9f97d7e10615e
Heather Conboy <hconboy@Heathers-MBP.hsd1.ma.comcast.net>
2022-10-22 17:38:02 -0400

    Added an initial version of the BasicStatsModel class to support the MVC architecture pattern. This class currently specifies the API but does not implement the method bodies.

---
+++ src/BasicStatsModel.java
@@ -0,0 +1,43 @@
+/**
+ * The BasicStatsModel class stores a list of numbers. This class provides
+ * the data representation for the MVC architecture pattern.
+ */
+public class BasicStatsModel
+{
+    public BasicStatsModel() {
+	super();
+    }
+
+    /**
+     * Adds the given number to the list of numbers.
+     *
+     * Post-condition: If the given number is non-null, the list of numbers
+     *                 contains that number.
+     *
+     * @param number The given number to be added to the list of numbers
+     * @exception IllegalArgumentException if the given number is null
+     */
+    public void addNumber(Double number) {
+	//TODO
+    }
+
+    /**
+     * Resets the list of numbers.
+     *
+     * Post-condition: The list of numbers is empty.
+     */
+    public void reset() {
+	//TODO
+    }
+
+    /**
+     * Converts from a list of doubles to an array of doubles.
+     *
+     * @return An array of doubles that contains all of numbers
+     *         in the list of doubles in the same order
+     */
+    public double[] getArrayDouble() {
+	//TODO
+	return null;
+    }
+}

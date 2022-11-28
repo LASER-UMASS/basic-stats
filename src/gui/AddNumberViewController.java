c005f6d00ecf035153cf2a545deed37f2479fe2a
Heather Conboy <hconboy@eduroamguest112.sys.cs.local>
2022-11-28 13:40:21 -0500

    Modified the AddNumberViewController constructor arguments

--- src/gui/AddNumberViewController.java
+++ src/gui/AddNumberViewController.java
@@ -12,7 +12,6 @@ import gui.view.View;
 
 public class AddNumberViewController implements View
 {
-    private BasicStatsModel model;
     private BasicStatsGUI gui;
     
     /** The 'Add number' view/controller allows the user to input a number */
@@ -20,8 +19,7 @@ public class AddNumberViewController implements View
     /* package */ JButton jbAdd;
 
     
-    public AddNumberViewController(BasicStatsModel model, BasicStatsGUI gui, JPanel jpInput) {
-	this.model = model;
+    public AddNumberViewController(BasicStatsGUI gui, JPanel jpInput) {
 	this.gui = gui;
 	jtfNumber = new JTextField(5);
 	jbAdd = new JButton("Add number");
@@ -31,12 +29,9 @@ public class AddNumberViewController implements View
 	
 	jbAdd.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-		    // Parse input and add number to the ArrayList
-		    
+		    // Parse input and add number to the ArrayList	    
 		    Double num = Double.parseDouble(jtfNumber.getText());
-		    model.addNumber(num);
-		    
-		    gui.update(model);
+		    gui.addNumber(num);
 		}
 	    });
     }

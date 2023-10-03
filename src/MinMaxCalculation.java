d5d28fb019fbb57ccd916e1ce8cbac5cfd6a9fac
vaishnavirshah <vaishnavirahulshah@gmail.com>
2023-10-03 07:20:21 -0400

    updated MinMaxCalculation.java

--- src/MinMaxCalculation.java
+++ src/MinMaxCalculation.java
@@ -1,8 +1,20 @@
-/*
- * 
- * 
- * min max calculation
- * 
- * 
- * 
- */
+public class MinMaxCalculation {
+    public static void main(String[] args) {
+        int[] numbers = { 10, 5, 8, 20, 3 };
+
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
+}

cd8edb44b408d05bb69b4add6730139c1b59a4eb
Hung Nguyen Pro <htn@vl965-172-31-31-59.wireless.umass.edu>
2024-02-23 10:18:49 -0500

    MinMax Implementation

--- MinMaxCalculation.java
+++ MinMaxCalculation.java
@@ -1,3 +1,21 @@
 public class MinMaxCalculation {
-    
+    public static double min(double ... numbers) {
+        double min = Double.MAX_VALUE;
+        for (double num : numbers) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    public static double max(double ... numbers) {
+        double max = Double.MIN_VALUE;
+        for (double num : numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
 }
